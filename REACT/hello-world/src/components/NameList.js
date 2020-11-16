41c953202d7e55bcbb8d5f68203bf8a37e7be3ff
manish7213 <manish.1si11cs057@gmail.com>
2020-11-16 07:13:58 +0530

    Fixed Unique prop warning

--- REACT/hello-world/src/components/NameList.js
+++ REACT/hello-world/src/components/NameList.js
@@ -22,7 +22,7 @@ function NameList() {
     }
 ]
 
-const personList = persons.map(person => <Person person = {person}/>)
+const personList = persons.map(person => <Person key = {person.id} person = {person}/>)
    
     return <div>{personList}</div>;
 }
